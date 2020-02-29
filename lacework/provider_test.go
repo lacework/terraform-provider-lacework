fbaf173e4b18f919c6102fe118987db319b4313a
mjunglw <james.jung@lacework.net>
2020-02-28 17:58:06 -0800

    initial commit setting up provider

---
+++ lacework/provider_test.go
@@ -0,0 +1,38 @@
+package lacework
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+var testAccProvider *schema.Provider
+var testAccProviders map[string]terraform.ResourceProvider
+
+func init() {
+	testAccProvider = Provider().(*schema.Provider)
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"lacework": testAccProvider,
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvider_impl(t *testing.T) {
+	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("LACEWORK_API_TOKEN"); v == "" {
+		t.Fatal("LACEWORK_API_TOKEN must be set for acceptance tests")
+	}
+	if v := os.Getenv("LACEWORK_ACCOUNT"); v == "" {
+		t.Fatal("LACEWORK_ACCOUNT must be set for acceptance tests")
+	}
+}
