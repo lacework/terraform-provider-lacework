a98557497e5d8db049da9d95fecd83c03c1b2911
Salim Afiune Maya <afiune@lacework.net>
2020-03-12 13:07:05 -0600

    feat(data_source): new lacework_api_token
    New data source called `lacework_api_token` that will allow users to
    access a Lacework API token, this data source will be used to integrate
    with other providers in the future.
    
    This new data source requires the credentials from the Lacework provider
    (`account`, `api_key` and `api_secret`)
    
    The simplest example should look like:
    ```
    provider "lacework" { }
    data "lacework_api_token" "current" { }
    ```
    
    Closes https://github.com/lacework/terraform-provider-lacework/issues/5
    
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- lacework/provider.go
+++ lacework/provider.go
@@ -1,37 +1,64 @@
 package lacework
 
 import (
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+
+	"github.com/lacework/go-sdk/api"
 )
 
-// Provider returns a terraform.ResourceProvider.
+// Provider returns a Lacework terraform.ResourceProvider
 func Provider() terraform.ResourceProvider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"api_token": {
-				Type:        schema.TypeString,
-				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("LACEWORK_API_TOKEN", nil),
-				Description: "Lacework API Token",
-			},
 			"account": {
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("LACEWORK_ACCOUNT", nil),
 				Description: "Lacework account subdomain of URL",
 			},
+			"api_key": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("LACEWORK_API_KEY", nil),
+				Description: "Lacework API access key",
+			},
+			"api_secret": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("LACEWORK_API_SECRET", nil),
+				Description: "Lacework API access secret",
+			},
 		},
 
 		ResourcesMap: map[string]*schema.Resource{},
 
-		DataSourcesMap: map[string]*schema.Resource{},
+		DataSourcesMap: map[string]*schema.Resource{
+			"lacework_api_token": dataSourceLaceworkApiToken(),
+		},
 
 		ConfigureFunc: providerConfigure,
 	}
 }
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
-	// TODO: configure Lacework API client here
-	return nil, nil
+	var (
+		account = d.Get("account").(string)
+		key     = d.Get("api_key").(string)
+		secret  = d.Get("api_secret").(string)
+	)
+
+	// create a new Lacework api client
+	lacework, err := api.NewClient(
+		account,
+		api.WithApiKeys(key, secret),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("[INFO] Lacework API client created successfully.")
+	return lacework, nil
 }
