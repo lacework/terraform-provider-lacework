07daa4f94a58baa961cb0b69c051a50892ac60c1
Salim Afiune <afiune@lacework.net>
2020-07-23 12:36:46 -0600

    release: version bump and CHANGELOG updates (#13)
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- lacework/version.go
+++ lacework/version.go
@@ -2,4 +2,4 @@ package lacework
 
 // TODO @afiune to figure out how to update our version in Travis
 // for now, we will do it manually until we automate it
-const version = "0.1.1"
+const version = "0.2.0"
