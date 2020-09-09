7e5dd1b5ccd4a1533d4418f1c0490467990b7972
Salim Afiune Maya <afiune@lacework.net>
2020-09-09 23:57:37 +0200

    chore: ran terraform fmt for all examples/
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- examples/resource_lacework_alert_channel_jira_cloud/main.tf
+++ examples/resource_lacework_alert_channel_jira_cloud/main.tf
@@ -1,15 +1,15 @@
-provider "lacework" { }
+provider "lacework" {}
 
 resource "lacework_alert_channel_jira_cloud" "example" {
-	name        = "My Jira Cloud Alert Channel Example"
-	jira_url    = "mycompany.atlassian.net"
-	issue_type  = "Bug"
-	project_key = "EXAMPLE"
-	username    = "my@username.com"
-	api_token   = "abcd1234"
+  name        = "My Jira Cloud Alert Channel Example"
+  jira_url    = "mycompany.atlassian.net"
+  issue_type  = "Bug"
+  project_key = "EXAMPLE"
+  username    = "my@username.com"
+  api_token   = "abcd1234"
 
-	group_issues_by = "Events"
-	custom_template_file = <<TEMPLATE
+  group_issues_by      = "Events"
+  custom_template_file = <<TEMPLATE
 {
     "fields": {
         "labels": [
