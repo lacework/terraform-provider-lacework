6f16fc33e5e8a732540a855b9517d71252bc253e
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2020-12-17 14:21:53 +0000

    feat(resource): add Webhook integration (#49)
    * feat(api): add Webhook integration
    * chore(deps): update github.com/lacework/go-sdk
    * docs: Added webhook to index tree
    * docs: Added link to lacework support doucmentation
    * fix(test): Fix incorrect reference to WebhookIntegration type

---
+++ lacework/resource_lacework_alert_channel_webhook.go
@@ -0,0 +1,208 @@
+package lacework
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/lacework/go-sdk/api"
+)
+
+func resourceLaceworkAlertChannelWebhook() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLaceworkAlertChannelWebhookCreate,
+		Read:   resourceLaceworkAlertChannelWebhookRead,
+		Update: resourceLaceworkAlertChannelWebhookUpdate,
+		Delete: resourceLaceworkAlertChannelWebhookDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: importLaceworkIntegration,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"intg_guid": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"webhook_url": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"created_or_updated_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"created_or_updated_by": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"type_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"org_level": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceLaceworkAlertChannelWebhookCreate(d *schema.ResourceData, meta interface{}) error {
+	var (
+		lacework = meta.(*api.Client)
+		webhook  = api.NewWebhookAlertChannel(d.Get("name").(string),
+			api.WebhookChannelData{
+				WebhookUrl: d.Get("webhook_url").(string),
+			},
+		)
+	)
+	if !d.Get("enabled").(bool) {
+		webhook.Enabled = 0
+	}
+
+	log.Printf("[INFO] Creating %s integration with data:\n%+v\n", api.WebhookIntegration, webhook)
+	response, err := lacework.Integrations.CreateWebhookAlertChannel(webhook)
+	if err != nil {
+		return err
+	}
+
+	log.Println("[INFO] Verifying server response data")
+	err = validateWebhookAlertChannelResponse(&response)
+	if err != nil {
+		return err
+	}
+
+	integration := response.Data[0]
+	d.SetId(integration.IntgGuid)
+	d.Set("name", integration.Name)
+	d.Set("intg_guid", integration.IntgGuid)
+	d.Set("enabled", integration.Enabled == 1)
+	d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
+	d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
+	d.Set("type_name", integration.TypeName)
+	d.Set("org_level", integration.IsOrg == 1)
+
+	log.Printf("[INFO] Created %s integration with guid: %v\n", api.WebhookIntegration, integration.IntgGuid)
+	return nil
+}
+
+func resourceLaceworkAlertChannelWebhookRead(d *schema.ResourceData, meta interface{}) error {
+	lacework := meta.(*api.Client)
+
+	log.Printf("[INFO] Reading %s integration with guid: %v\n", api.WebhookIntegration, d.Id())
+	response, err := lacework.Integrations.GetWebhookAlertChannel(d.Id())
+	if err != nil {
+		return err
+	}
+
+	for _, integration := range response.Data {
+		if integration.IntgGuid == d.Id() {
+			d.Set("name", integration.Name)
+			d.Set("intg_guid", integration.IntgGuid)
+			d.Set("enabled", integration.Enabled == 1)
+			d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
+			d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
+			d.Set("type_name", integration.TypeName)
+			d.Set("org_level", integration.IsOrg == 1)
+			d.Set("webhook_url", integration.Data.WebhookUrl)
+
+			log.Printf("[INFO] Read %s integration with guid: %v\n",
+				api.WebhookIntegration, integration.IntgGuid)
+			return nil
+		}
+	}
+
+	d.SetId("")
+	return nil
+}
+
+func resourceLaceworkAlertChannelWebhookUpdate(d *schema.ResourceData, meta interface{}) error {
+	var (
+		lacework = meta.(*api.Client)
+		webhook  = api.NewWebhookAlertChannel(d.Get("name").(string),
+			api.WebhookChannelData{
+				WebhookUrl: d.Get("webhook_url").(string),
+			},
+		)
+	)
+
+	if !d.Get("enabled").(bool) {
+		webhook.Enabled = 0
+	}
+
+	webhook.IntgGuid = d.Id()
+
+	log.Printf("[INFO] Updating %s integration with data:\n%+v\n", api.WebhookIntegration, webhook)
+	response, err := lacework.Integrations.UpdateWebhookAlertChannel(webhook)
+	if err != nil {
+		return err
+	}
+
+	log.Println("[INFO] Verifying server response data")
+	err = validateWebhookAlertChannelResponse(&response)
+	if err != nil {
+		return err
+	}
+
+	integration := response.Data[0]
+	d.Set("name", integration.Name)
+	d.Set("intg_guid", integration.IntgGuid)
+	d.Set("enabled", integration.Enabled == 1)
+	d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
+	d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
+	d.Set("type_name", integration.TypeName)
+	d.Set("org_level", integration.IsOrg == 1)
+
+	log.Printf("[INFO] Updated %s integration with guid: %v\n", api.WebhookIntegration, d.Id())
+	return nil
+}
+
+func resourceLaceworkAlertChannelWebhookDelete(d *schema.ResourceData, meta interface{}) error {
+	lacework := meta.(*api.Client)
+
+	log.Printf("[INFO] Deleting %s integration with guid: %v\n", api.WebhookIntegration, d.Id())
+	_, err := lacework.Integrations.Delete(d.Id())
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[INFO] Deleted %s integration with guid: %v\n", api.WebhookIntegration, d.Id())
+	return nil
+}
+
+func validateWebhookAlertChannelResponse(response *api.WebhookAlertChannelResponse) error {
+	if len(response.Data) == 0 {
+		msg := `
+Unable to read sever response data. (empty 'data' field)
+
+This was an unexpected behavior, verify that your integration has been
+created successfully and report this issue to support@lacework.net
+`
+		return fmt.Errorf(msg)
+	}
+
+	if len(response.Data) > 1 {
+		msg := `
+There is more that one integration inside the server response data.
+
+List of integrations:
+`
+		for _, integration := range response.Data {
+			msg = msg + fmt.Sprintf("\t%s: %s\n", integration.IntgGuid, integration.Name)
+		}
+		msg = msg + unexpectedBehaviorMsg()
+		return fmt.Errorf(msg)
+	}
+
+	return nil
+}
---
+++ website/docs/r/alert_channel_webhook.html.markdown
@@ -0,0 +1,43 @@
+---
+subcategory: "Alert Channels"
+layout: "lacework"
+page_title: "Lacework: lacework_alert_channel_webhook"
+description: |-
+  Create and manage Webhook Alert Channel integrations
+---
+
+# lacework\_alert\_channel\_webhook
+
+You can use this resource to create a custom webhook that receives Lacework alert notifications from a Lacework alert channel and forwards those alerts to a third-party application.
+
+-> **Note:**  If the third-party application receiving the HTTP POST request requires an API token, enter the API token as part of the URL eg. https://webhook.com?api-token=123
+
+To find more information about the alert payload, see the [Lacework support documentation](https://support.lacework.com/hc/en-us/articles/360034367393-Webhook).
+
+## Example Usage
+
+```hcl
+resource "lacework_alert_channel_" "ops_critical" {
+  name      = "OPS Critical Alerts"
+  webhook_url = "https://webhook.com?api-token=123"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) The Alert Channel integration name.
+* `webhook_url` - (Required) The URL of your webhook that will receive the HTTP POST.
+* `enabled` - (Optional) The state of the external integration. Defaults to `true`.
+
+## Import
+
+A Lacework Webhook Alert Channel integration can be imported using a `INT_GUID`, e.g.
+
+```
+$ terraform import lacework_alert_channel_webhook.ops_critical EXAMPLE_1234BAE1E42182964D23973F44CFEA3C4AB63B99E9A1EC5
+```
+-> **Note:** To retreive the `INT_GUID` from existing integrations in your account, use the
+	Lacework CLI command `lacework integration list`. To install this tool follow
+	[this documentation](https://github.com/lacework/go-sdk/wiki/CLI-Documentation#installation).
