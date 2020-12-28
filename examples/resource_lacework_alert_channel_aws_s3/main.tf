d3c8b63ece48817f07c7cb4de3397c3ef765d1a3
Salim Afiune Maya <afiune@lacework.net>
2020-12-28 09:35:03 +0100

    chore: format AWS s3 alert channel example
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- examples/resource_lacework_alert_channel_aws_s3/main.tf
+++ examples/resource_lacework_alert_channel_aws_s3/main.tf
@@ -1,8 +1,8 @@
 provider "lacework" {}
 
 resource "lacework_alert_channel_aws_s3" "s3_export" {
-  name        = "S3 Data Export"
-  bucket_arn  = "arn:aws:s3:::bucket_name/key_name"
+  name       = "S3 Data Export"
+  bucket_arn = "arn:aws:s3:::bucket_name/key_name"
   credentials {
     external_id = "12345"
     role_arn    = "arn:aws:iam::1234567890:role/lacework_iam_example_role"
