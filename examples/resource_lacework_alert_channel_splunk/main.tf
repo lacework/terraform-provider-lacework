066fa363cef4d224e995ee11b39aa7872eb1859b
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2021-01-14 13:04:17 +0000

    style: Change example token in splunk tf example
    Co-authored-by: Salim Afiune <afiune@lacework.net>

--- examples/resource_lacework_alert_channel_splunk/main.tf
+++ examples/resource_lacework_alert_channel_splunk/main.tf
@@ -2,7 +2,7 @@ provider "lacework" {}
 
 resource "lacework_alert_channel_splunk" "example" {
   name      = "Splunk Channel Alert Example"
-  hec_token = "AA111111-11AA-1AA1-11AA-11111AA1111A"
+  hec_token = "BA696D5E-CA2F-4347-97CB-3C89F834816F"
   host = "host"
   port = 80
   event_data {
