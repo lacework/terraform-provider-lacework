b3c28cc0c7583b1a15f5c76d32d96f021595aec5
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2021-01-26 10:24:52 +0000

    style: fix indentation

--- examples/resource_lacework_alert_channel_gcp_pub_sub/main.tf
+++ examples/resource_lacework_alert_channel_gcp_pub_sub/main.tf
@@ -1,14 +1,14 @@
 provider "lacework" {}
 
 resource "lacework_alert_channel_gcp_pub_sub" "example" {
-  name       = "Gcp Pub Sub example"
-  project_id = "my-sample-project-191923"
-  topic_id   = "mytopic"
-  issue_grouping   = "Events"
+  name           = "Gcp Pub Sub example"
+  project_id     = "my-sample-project-191923"
+  topic_id       = "mytopic"
+  issue_grouping = "Events"
   credentials {
-    client_id = "client_id"
-    client_email = "foo@example.iam.gserviceaccount.com"
-    private_key = "priv_key"
+    client_id      = "client_id"
+    client_email   = "foo@example.iam.gserviceaccount.com"
+    private_key    = "priv_key"
     private_key_id = "p_key_id"
   }
 }
