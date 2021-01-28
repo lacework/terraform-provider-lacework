568a097b20f1a503361e383645f8e0f8b17db5e1
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2021-01-28 17:49:19 +0000

    feat: new ServiceNow alert channel resource (#63)
    * feat: new ServiceNow alert channel resource
    
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

---
+++ lacework/resource_lacework_alert_channel_service_now.go
@@ -0,0 +1,242 @@
+package lacework
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/lacework/go-sdk/api"
+)
+
+func resourceLaceworkAlertChannelServiceNow() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLaceworkAlertChannelServiceNowCreate,
+		Read:   resourceLaceworkAlertChannelServiceNowRead,
+		Update: resourceLaceworkAlertChannelServiceNowUpdate,
+		Delete: resourceLaceworkAlertChannelServiceNowDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: importLaceworkIntegration,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"intg_guid": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"instance_url": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"username": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"password": {
+				Type:      schema.TypeString,
+				Required:  true,
+				Sensitive: true,
+			},
+			"issue_grouping": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "Events",
+				ValidateFunc: func(value interface{}, key string) ([]string, []error) {
+					switch value.(string) {
+					case "Events", "Resources":
+						return nil, nil
+					default:
+						return nil, []error{
+							fmt.Errorf(
+								"%s: can only be either 'Events' or 'Resources' (default: Events)", key,
+							),
+						}
+					}
+				},
+			},
+			"created_or_updated_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"created_or_updated_by": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"type_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"org_level": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceLaceworkAlertChannelServiceNowCreate(d *schema.ResourceData, meta interface{}) error {
+	var (
+		lacework   = meta.(*api.Client)
+		serviceNow = api.NewServiceNowAlertChannel(d.Get("name").(string),
+			api.ServiceNowChannelData{
+				InstanceURL:   d.Get("instanceURL").(string),
+				Username:      d.Get("username").(string),
+				Password:      d.Get("password").(string),
+				IssueGrouping: d.Get("issue_grouping").(string),
+			},
+		)
+	)
+	if !d.Get("enabled").(bool) {
+		serviceNow.Enabled = 0
+	}
+
+	log.Printf("[INFO] Creating %s integration with data:\n%+v\n", api.ServiceNowChannelIntegration, serviceNow)
+	response, err := lacework.Integrations.CreateServiceNowAlertChannel(serviceNow)
+	if err != nil {
+		return err
+	}
+
+	log.Println("[INFO] Verifying server response data")
+	err = validateServiceNowAlertChannelResponse(&response)
+	if err != nil {
+		return err
+	}
+
+	integration := response.Data[0]
+	d.SetId(integration.IntgGuid)
+	d.Set("name", integration.Name)
+	d.Set("intg_guid", integration.IntgGuid)
+	d.Set("enabled", integration.Enabled == 1)
+	d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
+	d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
+	d.Set("type_name", integration.TypeName)
+	d.Set("org_level", integration.IsOrg == 1)
+
+	log.Printf("[INFO] Created %s integration with guid: %v\n", api.ServiceNowChannelIntegration, integration.IntgGuid)
+	return nil
+}
+
+func resourceLaceworkAlertChannelServiceNowRead(d *schema.ResourceData, meta interface{}) error {
+	lacework := meta.(*api.Client)
+
+	log.Printf("[INFO] Reading %s integration with guid: %v\n", api.ServiceNowChannelIntegration, d.Id())
+	response, err := lacework.Integrations.GetServiceNowAlertChannel(d.Id())
+	if err != nil {
+		return err
+	}
+
+	for _, integration := range response.Data {
+		if integration.IntgGuid == d.Id() {
+			d.Set("name", integration.Name)
+			d.Set("intg_guid", integration.IntgGuid)
+			d.Set("enabled", integration.Enabled == 1)
+			d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
+			d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
+			d.Set("type_name", integration.TypeName)
+			d.Set("org_level", integration.IsOrg == 1)
+			d.Set("instance_url", integration.Data.InstanceURL)
+			d.Set("username", integration.Data.Username)
+			d.Set("issue_grouping", integration.Data.IssueGrouping)
+
+			log.Printf("[INFO] Read %s integration with guid: %v\n",
+				api.ServiceNowChannelIntegration, integration.IntgGuid)
+			return nil
+		}
+	}
+
+	d.SetId("")
+	return nil
+}
+
+func resourceLaceworkAlertChannelServiceNowUpdate(d *schema.ResourceData, meta interface{}) error {
+	var (
+		lacework   = meta.(*api.Client)
+		serviceNow = api.NewServiceNowAlertChannel(d.Get("name").(string),
+			api.ServiceNowChannelData{
+				InstanceURL:   d.Get("instanceURL").(string),
+				Username:      d.Get("username").(string),
+				Password:      d.Get("password").(string),
+				IssueGrouping: d.Get("issue_grouping").(string),
+			},
+		)
+	)
+
+	if !d.Get("enabled").(bool) {
+		serviceNow.Enabled = 0
+	}
+
+	serviceNow.IntgGuid = d.Id()
+
+	log.Printf("[INFO] Updating %s integration with data:\n%+v\n", api.ServiceNowChannelIntegration, serviceNow)
+	response, err := lacework.Integrations.UpdateServiceNowAlertChannel(serviceNow)
+	if err != nil {
+		return err
+	}
+
+	log.Println("[INFO] Verifying server response data")
+	err = validateServiceNowAlertChannelResponse(&response)
+	if err != nil {
+		return err
+	}
+
+	integration := response.Data[0]
+	d.Set("name", integration.Name)
+	d.Set("intg_guid", integration.IntgGuid)
+	d.Set("enabled", integration.Enabled == 1)
+	d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
+	d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
+	d.Set("type_name", integration.TypeName)
+	d.Set("org_level", integration.IsOrg == 1)
+
+	log.Printf("[INFO] Updated %s integration with guid: %v\n", api.ServiceNowChannelIntegration, d.Id())
+	return nil
+}
+
+func resourceLaceworkAlertChannelServiceNowDelete(d *schema.ResourceData, meta interface{}) error {
+	lacework := meta.(*api.Client)
+
+	log.Printf("[INFO] Deleting %s integration with guid: %v\n", api.ServiceNowChannelIntegration, d.Id())
+	_, err := lacework.Integrations.Delete(d.Id())
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[INFO] Deleted %s integration with guid: %v\n", api.ServiceNowChannelIntegration, d.Id())
+	return nil
+}
+
+func validateServiceNowAlertChannelResponse(response *api.ServiceNowAlertChannelResponse) error {
+	if len(response.Data) == 0 {
+		msg := `
+Unable to read sever response data. (empty 'data' field)
+
+This was an unexpected behavior, verify that your integration has been
+created successfully and report this issue to support@lacework.net
+`
+		return fmt.Errorf(msg)
+	}
+
+	if len(response.Data) > 1 {
+		msg := `
+There is more that one integration inside the server response data.
+
+List of integrations:
+`
+		for _, integration := range response.Data {
+			msg = msg + fmt.Sprintf("\t%s: %s\n", integration.IntgGuid, integration.Name)
+		}
+		msg = msg + unexpectedBehaviorMsg()
+		return fmt.Errorf(msg)
+	}
+
+	return nil
+}
---
+++ lacework/resource_lacework_alert_channel_service_now_test.go
@@ -0,0 +1,159 @@
+package lacework
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/lacework/go-sdk/api"
+)
+
+const (
+	testAccAlertChannelServiceNowResourceType = "lacework_alert_channel_splunk"
+	testAccAlertChannelServiceNowResourceName = "example"
+
+	testAccAlertChannelServiceNowChannel       = "SERVICE_NOW_REST"
+	testAccAlertChannelServiceNowInstanceURL   = "INSTANCE_URL"
+	testAccAlertChannelServiceNowUsername      = "USERNAME"
+	testAccAlertChannelServiceNowPassword      = "PASSWORD"
+	testAccAlertChannelServiceNowIssueGrouping = "ISSUE_GROUPING"
+)
+
+func TestAccAlertChannelServiceNow(t *testing.T) {
+	resourceTypeAndName := fmt.Sprintf("%s.%s",
+		testAccAlertChannelServiceNowResourceType,
+		testAccAlertChannelServiceNowResourceName,
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccAlertChannelServiceNowEnvVarsPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAlertChannelServiceNowDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAlertChannelServiceNowConfig(
+					true,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlertChannelServiceNowExists(resourceTypeAndName),
+					resource.TestCheckResourceAttr(resourceTypeAndName, "enabled", "true"),
+				),
+			},
+			{
+				Config: testAccAlertChannelServiceNowConfig(
+					false,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlertChannelServiceNowExists(resourceTypeAndName),
+					resource.TestCheckResourceAttr(resourceTypeAndName, "enabled", "false"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAlertChannelServiceNowDestroy(s *terraform.State) error {
+	lacework := testAccProvider.Meta().(*api.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != testAccAlertChannelServiceNowResourceType {
+			continue
+		}
+
+		response, err := lacework.Integrations.GetServiceNowAlertChannel(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		for _, integration := range response.Data {
+			if integration.IntgGuid == rs.Primary.ID {
+				return fmt.Errorf(
+					"the %s integration (%s) still exists",
+					api.ServiceNowChannelIntegration, rs.Primary.ID,
+				)
+			}
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckAlertChannelServiceNowExists(resourceTypeAndName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		lacework := testAccProvider.Meta().(*api.Client)
+
+		rs, ok := s.RootModule().Resources[resourceTypeAndName]
+		if !ok {
+			return fmt.Errorf("resource (%s) not found", resourceTypeAndName)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("resource (%s) ID not set", resourceTypeAndName)
+		}
+
+		response, err := lacework.Integrations.GetServiceNowAlertChannel(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		if len(response.Data) < 1 {
+			return fmt.Errorf("the %s integration (%s) doesn't exist",
+				api.ServiceNowChannelIntegration, rs.Primary.ID)
+		}
+
+		for _, integration := range response.Data {
+			if integration.IntgGuid == rs.Primary.ID {
+				return nil
+			}
+		}
+
+		return fmt.Errorf("the %s integration (%s) doesn't exist",
+			api.ServiceNowChannelIntegration, rs.Primary.ID)
+	}
+}
+
+func testAccAlertChannelServiceNowEnvVarsPreCheck(t *testing.T) {
+	if v := os.Getenv(testAccAlertChannelServiceNowChannel); v == "" {
+		t.Fatalf("%s must be set for acceptance tests", testAccAlertChannelServiceNowChannel)
+	}
+	if v := os.Getenv(testAccAlertChannelServiceNowInstanceURL); v == "" {
+		t.Fatalf("%s must be set for acceptance tests", testAccAlertChannelServiceNowInstanceURL)
+	}
+	if v := os.Getenv(testAccAlertChannelServiceNowUsername); v == "" {
+		t.Fatalf("%s must be set for acceptance tests", testAccAlertChannelServiceNowUsername)
+	}
+	if v := os.Getenv(testAccAlertChannelServiceNowPassword); v == "" {
+		t.Fatalf("%s must be set for acceptance tests", testAccAlertChannelServiceNowPassword)
+	}
+	if v := os.Getenv(testAccAlertChannelServiceNowIssueGrouping); v == "" {
+		t.Fatalf("%s must be set for acceptance tests", testAccAlertChannelServiceNowIssueGrouping)
+	}
+}
+
+func testAccAlertChannelServiceNowConfig(enabled bool) string {
+	return fmt.Sprintf(`
+	resource "%s" "%s" {
+		name = "integration test"
+		enabled = %t
+		channel = "%s"
+		instance_url = "%s"
+		username = "%s"
+		password = "%s"
+		issue_grouping = "%s"
+	}
+	`,
+		testAccAlertChannelServiceNowResourceType,
+		testAccAlertChannelServiceNowResourceName,
+		enabled,
+		os.Getenv(testAccAlertChannelServiceNowChannel),
+		os.Getenv(testAccAlertChannelServiceNowInstanceURL),
+		os.Getenv(testAccAlertChannelServiceNowUsername),
+		os.Getenv(testAccAlertChannelServiceNowPassword),
+		os.Getenv(testAccAlertChannelServiceNowIssueGrouping),
+	)
+}
