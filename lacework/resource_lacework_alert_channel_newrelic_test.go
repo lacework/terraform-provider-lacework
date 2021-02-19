6be7b036bf5f72aac4804cba898274ca4f05d7c0
Salim Afiune <afiune@lacework.net>
2021-02-19 08:08:18 -0600

    Update lacework/resource_lacework_alert_channel_newrelic_test.go

--- lacework/resource_lacework_alert_channel_newrelic_test.go
+++ lacework/resource_lacework_alert_channel_newrelic_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	testAccAlertChannelNewRelicResourceType = "lacework_alert_channel_new_relic"
+	testAccAlertChannelNewRelicResourceType = "lacework_alert_channel_newrelic"
 	testAccAlertChannelNewRelicResourceName = "example"
 
 	testAccAlertChannelNewRelicAccountID = "ACCOUNT_ID"
