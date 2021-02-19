5d7877a9da5155cde1da58de00cbc37cb789f4e9
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2021-02-19 14:28:54 +0000

    feat: New Relic Insights alert channel resource (#73)
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

---
+++ lacework/resource_lacework_alert_channel_newrelic_test.go
@@ -0,0 +1,141 @@
+package lacework
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/lacework/go-sdk/api"
+)
+
+const (
+	testAccAlertChannelNewRelicResourceType = "lacework_alert_channel_newrelic"
+	testAccAlertChannelNewRelicResourceName = "example"
+
+	testAccAlertChannelNewRelicAccountID = "ACCOUNT_ID"
+	testAccAlertChannelNewRelicInsertKey = "INSERT_KEY"
+)
+
+func TestAccAlertChannelNewRelic(t *testing.T) {
+	resourceTypeAndName := fmt.Sprintf("%s.%s",
+		testAccAlertChannelNewRelicResourceType,
+		testAccAlertChannelNewRelicResourceName,
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccAlertChannelNewRelicEnvVarsPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAlertChannelNewRelicDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAlertChannelNewRelicConfig(
+					true,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlertChannelNewRelicExists(resourceTypeAndName),
+					resource.TestCheckResourceAttr(resourceTypeAndName, "enabled", "true"),
+				),
+			},
+			{
+				Config: testAccAlertChannelNewRelicConfig(
+					false,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlertChannelNewRelicExists(resourceTypeAndName),
+					resource.TestCheckResourceAttr(resourceTypeAndName, "enabled", "false"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAlertChannelNewRelicDestroy(s *terraform.State) error {
+	lacework := testAccProvider.Meta().(*api.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != testAccAlertChannelNewRelicResourceType {
+			continue
+		}
+
+		response, err := lacework.Integrations.GetNewRelicAlertChannel(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		for _, integration := range response.Data {
+			if integration.IntgGuid == rs.Primary.ID {
+				return fmt.Errorf(
+					"the %s integration (%s) still exists",
+					api.NewRelicChannelIntegration, rs.Primary.ID,
+				)
+			}
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckAlertChannelNewRelicExists(resourceTypeAndName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		lacework := testAccProvider.Meta().(*api.Client)
+
+		rs, ok := s.RootModule().Resources[resourceTypeAndName]
+		if !ok {
+			return fmt.Errorf("resource (%s) not found", resourceTypeAndName)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("resource (%s) ID not set", resourceTypeAndName)
+		}
+
+		response, err := lacework.Integrations.GetNewRelicAlertChannel(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		if len(response.Data) < 1 {
+			return fmt.Errorf("the %s integration (%s) doesn't exist",
+				api.NewRelicChannelIntegration, rs.Primary.ID)
+		}
+
+		for _, integration := range response.Data {
+			if integration.IntgGuid == rs.Primary.ID {
+				return nil
+			}
+		}
+
+		return fmt.Errorf("the %s integration (%s) doesn't exist",
+			api.NewRelicChannelIntegration, rs.Primary.ID)
+	}
+}
+
+func testAccAlertChannelNewRelicEnvVarsPreCheck(t *testing.T) {
+	if v := os.Getenv(testAccAlertChannelNewRelicAccountID); v == "" {
+		t.Fatalf("%s must be set for acceptance tests", testAccAlertChannelNewRelicAccountID)
+	}
+	if v := os.Getenv(testAccAlertChannelNewRelicInsertKey); v == "" {
+		t.Fatalf("%s must be set for acceptance tests", testAccAlertChannelNewRelicInsertKey)
+	}
+}
+
+func testAccAlertChannelNewRelicConfig(enabled bool) string {
+	return fmt.Sprintf(`
+	resource "%s" "%s" {
+		name = "integration test"
+		enabled = %t
+		account_id = "%s"
+		insert_key = "%s"
+	}
+	`,
+		testAccAlertChannelNewRelicResourceType,
+		testAccAlertChannelNewRelicResourceName,
+		enabled,
+		os.Getenv(testAccAlertChannelNewRelicAccountID),
+		os.Getenv(testAccAlertChannelNewRelicInsertKey),
+	)
+}
