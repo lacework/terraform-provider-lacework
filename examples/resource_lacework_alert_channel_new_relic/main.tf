2b80a0ac85a3cee06a8d8ac4609ec7240e56e1a9
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2021-02-19 11:11:21 +0000

    style: formatting
    Co-authored-by: Salim Afiune <afiune@lacework.net>

--- examples/resource_lacework_alert_channel_new_relic/main.tf
+++ examples/resource_lacework_alert_channel_new_relic/main.tf
@@ -1,7 +1,7 @@
 provider "lacework" {}
 
 resource "lacework_alert_channel_new_relic" "example" {
-  name      = "My New Relic Insights Channel Alert Example"
+  name       = "My New Relic Insights Channel Alert Example"
   account_id = 2338053
   insert_key = "x-xx-xxxxxxxxxxxxxxxxxx"
 }
