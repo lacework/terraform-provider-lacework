d84cc51a1f72e5bd7575f32b1609a1d4766284b0
Salim Afiune Maya <afiune@lacework.net>
2021-08-17 18:54:02 -0500

    fix: migrate Slack Alert Channels to use API v2
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

---
+++ integration/resource_lacework_alert_channel_slack_test.go
@@ -0,0 +1,31 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestAlertChannelSlackCreate applies integration terraform:
+// => '../examples/resource_lacework_alert_channel_slack'
+//
+// It uses the go-sdk to verify the created integration,
+// applies an update with new alert channel name and destroys it
+func TestAlertChannelSlackCreate(t *testing.T) {
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../examples/resource_lacework_alert_channel_slack",
+	})
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Create new Slack Alert Channel
+	create := terraform.InitAndApply(t, terraformOptions)
+	assert.Equal(t, "Slack Alert Channel Example", GetIntegrationName(create))
+
+	// Update Slack Alert Channel
+	terraformOptions.Vars = map[string]interface{}{
+		"channel_name": "Slack Alert Channel Updated"}
+
+	update := terraform.Apply(t, terraformOptions)
+	assert.Equal(t, "Slack Alert Channel Updated", GetIntegrationName(update))
+}
