4433d9a10367c49b412a399d5358f96bb95b51f5
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2021-09-24 10:50:09 +0100

    feat: New Machine Resource Group Terraform Resource (#169)
    * feat: New Machine Resource Group Terraform Resource
    
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

---
+++ integration/resource_lacework_resource_group_machine_test.go
@@ -0,0 +1,40 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestResourceGroupMachineCreate applies integration terraform:
+// => '../examples/resource_lacework_resource_group_machine'
+//
+// It uses the go-sdk to verify the created resource group,
+// applies an update with new description and destroys it
+func TestResourceGroupMachineCreate(t *testing.T) {
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../examples/resource_lacework_resource_group_machine",
+		Vars: map[string]interface{}{
+			"description":   "Terraform Test Machine Resource Group",
+			"machine_key":   "test-key",
+			"machine_value": "test-value",
+		},
+	})
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Create new Machine Resource Group
+	create := terraform.InitAndApplyAndIdempotent(t, terraformOptions)
+	createProps := GetMachineResourceGroupProps(create)
+	assert.Equal(t, "Terraform Test Machine Resource Group", createProps.Description)
+	assert.Equal(t, []map[string]string{{"test-key": "test-value"}}, createProps.MachineTags)
+
+	// Update Machine Resource Group
+	terraformOptions.Vars["description"] = "Updated Terraform Test Machine Resource Group"
+	terraformOptions.Vars["machine_value"] = "updated-value"
+
+	update := terraform.ApplyAndIdempotent(t, terraformOptions)
+	updateProps := GetMachineResourceGroupProps(update)
+	assert.Equal(t, "Updated Terraform Test Machine Resource Group", updateProps.Description)
+	assert.Equal(t, []map[string]string{{"test-key": "updated-value"}}, updateProps.MachineTags)
+}
