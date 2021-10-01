4af5307ef9a5eb7c3882f2a7e1acbc4502f5fabd
Vatasha White <vatasha.white@lacework.net>
2021-10-01 09:25:48 -0700

    fix: Migrate cloudwatch alert channel to API v2

--- examples/resource_lacework_alert_channel_aws_cloudwatch/main.tf
+++ examples/resource_lacework_alert_channel_aws_cloudwatch/main.tf
@@ -1,7 +1,20 @@
+terraform {
+  required_providers {
+    lacework = {
+      source = "lacework/lacework"
+    }
+  }
+}
+
 provider "lacework" {}
 
 resource "lacework_alert_channel_aws_cloudwatch" "example" {
   name            = "My AWS CloudWatch Alert Channel Example"
   event_bus_arn   = "arn:aws:events:us-west-2:1234567890:event-bus/default"
-  group_issues_by = "Resources"
+  group_issues_by = "Events"
+  // test_integration input is used in this example only for testing
+  // purposes, it help us avoid sending a "test" request to the
+  // system we are integrating to. In production, this should remain
+  // turned on ("true") which is the default setting
+  test_integration = false
 }
