3462e7ff87df9d55f52ef9248dce5e818f9e4072
Darren Murray <darren.murray@lacework.net>
2021-10-12 11:57:27 +0100

    feat: Migrate VictorOps alert channel(v2)
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

---
+++ examples/resource_lacework_alert_channel_victorops/variables.tf
@@ -0,0 +1,8 @@
+variable "channel_name" {
+  type    = string
+  default = "VictorOps Alert Channel"
+}
+variable "webhook_url" {
+  type    = string
+  default = "https://alert.victorops.com/integrations/generic/20131114/alert/31e945ee-5cad-44e7-afb0-97c20ea80dd8/database"
+}
---
+++ integration/resource_lacework_integration_victorops_test.go
@@ -0,0 +1,39 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestVictorOpsAlertChannelCreate applies integration terraform '../examples/resource_lacework_alert_channel_victorops'
+// Uses the go-sdk to verify the created integration
+// Applies an update with new channel name and Terraform destroy
+func TestVictorOpsAlertChannelCreate(t *testing.T) {
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../examples/resource_lacework_alert_channel_victorops",
+		Vars: map[string]interface{}{
+			"channel_name": "VictorOps Alert Channel Example",
+			"webhook_url":  "https://alert.victorops.com/integrations/generic/20131114/alert/31e945ee-5cad-44e7-afb0-97c20ea80dd8/database",
+		},
+	})
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Create new VictorOps Alert Channel
+	create := terraform.InitAndApplyAndIdempotent(t, terraformOptions)
+	assert.Equal(t, "VictorOps Alert Channel Example", GetIntegrationName(create))
+
+	// Update VictorOps Alert Channel
+	terraformOptions.Vars = map[string]interface{}{
+		"channel_name": "VictorOps Alert Channel Updated",
+		"webhook_url":  "https://alert.victorops.com/integrations/generic/12331114/alert/31e945ee-5cad-44e7-afb0-97c20ea80dd8/database"}
+
+	update := terraform.Apply(t, terraformOptions)
+
+	actualName := terraform.Output(t, terraformOptions, "channel_name")
+	actualUrl := terraform.Output(t, terraformOptions, "webhook_url")
+	assert.Equal(t, "VictorOps Alert Channel Updated", GetIntegrationName(update))
+	assert.Equal(t, "VictorOps Alert Channel Updated", actualName)
+	assert.Equal(t, "https://alert.victorops.com/integrations/generic/12331114/alert/31e945ee-5cad-44e7-afb0-97c20ea80dd8/database", actualUrl)
+}
