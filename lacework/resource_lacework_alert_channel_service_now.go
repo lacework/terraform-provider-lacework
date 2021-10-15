633cbfe6dce974a77721eb1765a3c799fb685893
Darren Murray <darren.murray@lacework.net>
2021-10-15 10:20:39 +0100

    fix: Migrate ServiceNow alert channel(v2)
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- lacework/resource_lacework_alert_channel_service_now.go
+++ lacework/resource_lacework_alert_channel_service_now.go
@@ -16,40 +16,42 @@ func resourceLaceworkAlertChannelServiceNow() *schema.Resource {
 		Delete: resourceLaceworkAlertChannelServiceNowDelete,
 
 		Importer: &schema.ResourceImporter{
-			State: importLaceworkIntegration,
+			State: importLaceworkAlertChannel,
 		},
 
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"intg_guid": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The integration name",
 			},
 			"enabled": {
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  true,
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     true,
+				Description: "The state of the external integration",
 			},
 			"instance_url": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The ServiceNow instance URL",
 			},
 			"username": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The ServiceNow username",
 			},
 			"password": {
-				Type:      schema.TypeString,
-				Required:  true,
-				Sensitive: true,
+				Type:        schema.TypeString,
+				Required:    true,
+				Sensitive:   true,
+				Description: "The ServiceNow password",
 			},
 			"issue_grouping": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Default:  "Events",
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "Events",
+				Description: "Defines how Lacework compliance events get grouped. Must be one of Events or Resources",
 				ValidateFunc: func(value interface{}, key string) ([]string, []error) {
 					switch value.(string) {
 					case "Events", "Resources":
@@ -64,14 +66,19 @@ func resourceLaceworkAlertChannelServiceNow() *schema.Resource {
 				},
 			},
 			"custom_template_file": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Populate fields in the ServiceNow incident with values from a custom template JSON file",
 			},
 			"test_integration": {
 				Type:        schema.TypeBool,
 				Optional:    true,
 				Default:     true,
 				Description: "Whether to test the integration of an alert channel upon creation and modification",
+			},
+			"intg_guid": {
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 			"created_or_updated_time": {
 				Type:     schema.TypeString,
@@ -97,7 +104,7 @@ func resourceLaceworkAlertChannelServiceNowCreate(d *schema.ResourceData, meta i
 	var (
 		lacework           = meta.(*api.Client)
 		customTemplateJSON = d.Get("custom_template_file").(string)
-		snowData           = api.ServiceNowChannelData{
+		snowData           = api.ServiceNowRestDataV2{
 			InstanceURL:   d.Get("instance_url").(string),
 			Username:      d.Get("username").(string),
 			Password:      d.Get("password").(string),
@@ -109,74 +116,55 @@ func resourceLaceworkAlertChannelServiceNowCreate(d *schema.ResourceData, meta i
 		snowData.EncodeCustomTemplateFile(customTemplateJSON)
 	}
 
-	serviceNow := api.NewServiceNowAlertChannel(d.Get("name").(string), snowData)
+	serviceNow := api.NewAlertChannel(d.Get("name").(string),
+		api.ServiceNowRestAlertChannelType,
+		snowData)
 	if !d.Get("enabled").(bool) {
 		serviceNow.Enabled = 0
 	}
 
-	log.Printf("[INFO] Creating %s integration with data:\n%+v\n", api.ServiceNowChannelIntegration, serviceNow)
-	response, err := lacework.Integrations.CreateServiceNowAlertChannel(serviceNow)
+	log.Printf("[INFO] Creating %s integration with data:\n%+v\n", api.ServiceNowRestAlertChannelType, serviceNow)
+	response, err := lacework.V2.AlertChannels.Create(serviceNow)
 	if err != nil {
 		return err
 	}
 
-	log.Println("[INFO] Verifying server response data")
-	err = validateServiceNowAlertChannelResponse(&response)
-	if err != nil {
-		return err
-	}
-
-	integration := response.Data[0]
+	integration := response.Data
 	d.SetId(integration.IntgGuid)
 	d.Set("name", integration.Name)
 	d.Set("intg_guid", integration.IntgGuid)
 	d.Set("enabled", integration.Enabled == 1)
 	d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
 	d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
-	d.Set("type_name", integration.TypeName)
+	d.Set("type_name", integration.Type)
 	d.Set("org_level", integration.IsOrg == 1)
 
-	if d.Get("test_integration").(bool) {
-		log.Printf("[INFO] Testing %s integration for guid %s\n", api.ServiceNowChannelIntegration, d.Id())
-		if err := VerifyAlertChannelAndRollback(d, lacework); err != nil {
-			return err
-		}
-		log.Printf("[INFO] Tested %s integration with guid %s successfully\n", api.ServiceNowChannelIntegration, d.Id())
-	}
-
-	log.Printf("[INFO] Created %s integration with guid %s\n", api.ServiceNowChannelIntegration, integration.IntgGuid)
+	log.Printf("[INFO] Created %s integration with guid %s\n", api.ServiceNowRestAlertChannelType, integration.IntgGuid)
 	return nil
 }
 
 func resourceLaceworkAlertChannelServiceNowRead(d *schema.ResourceData, meta interface{}) error {
 	lacework := meta.(*api.Client)
 
-	log.Printf("[INFO] Reading %s integration with guid %s\n", api.ServiceNowChannelIntegration, d.Id())
-	response, err := lacework.Integrations.GetServiceNowAlertChannel(d.Id())
+	log.Printf("[INFO] Reading %s integration with guid %s\n", api.ServiceNowRestAlertChannelType, d.Id())
+	response, err := lacework.V2.AlertChannels.GetServiceNowRest(d.Id())
 	if err != nil {
 		return err
 	}
+	integration := response.Data
+	d.Set("name", integration.Name)
+	d.Set("intg_guid", integration.IntgGuid)
+	d.Set("enabled", integration.Enabled == 1)
+	d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
+	d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
+	d.Set("type_name", integration.Type)
+	d.Set("org_level", integration.IsOrg == 1)
+	d.Set("instance_url", integration.Data.InstanceURL)
+	d.Set("username", integration.Data.Username)
+	d.Set("issue_grouping", integration.Data.IssueGrouping)
 
-	for _, integration := range response.Data {
-		if integration.IntgGuid == d.Id() {
-			d.Set("name", integration.Name)
-			d.Set("intg_guid", integration.IntgGuid)
-			d.Set("enabled", integration.Enabled == 1)
-			d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
-			d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
-			d.Set("type_name", integration.TypeName)
-			d.Set("org_level", integration.IsOrg == 1)
-			d.Set("instance_url", integration.Data.InstanceURL)
-			d.Set("username", integration.Data.Username)
-			d.Set("issue_grouping", integration.Data.IssueGrouping)
-
-			log.Printf("[INFO] Read %s integration with guid %s\n",
-				api.ServiceNowChannelIntegration, integration.IntgGuid)
-			return nil
-		}
-	}
-
-	d.SetId("")
+	log.Printf("[INFO] Read %s integration with guid %s\n",
+		api.ServiceNowRestAlertChannelType, integration.IntgGuid)
 	return nil
 }
 
@@ -184,7 +172,7 @@ func resourceLaceworkAlertChannelServiceNowUpdate(d *schema.ResourceData, meta i
 	var (
 		lacework           = meta.(*api.Client)
 		customTemplateJSON = d.Get("custom_template_file").(string)
-		snowData           = api.ServiceNowChannelData{
+		snowData           = api.ServiceNowRestDataV2{
 			InstanceURL:   d.Get("instance_url").(string),
 			Username:      d.Get("username").(string),
 			Password:      d.Get("password").(string),
@@ -196,82 +184,51 @@ func resourceLaceworkAlertChannelServiceNowUpdate(d *schema.ResourceData, meta i
 		snowData.EncodeCustomTemplateFile(customTemplateJSON)
 	}
 
-	serviceNow := api.NewServiceNowAlertChannel(d.Get("name").(string), snowData)
+	serviceNow := api.NewAlertChannel(d.Get("name").(string),
+		api.ServiceNowRestAlertChannelType,
+		snowData)
 	if !d.Get("enabled").(bool) {
 		serviceNow.Enabled = 0
 	}
 
 	serviceNow.IntgGuid = d.Id()
 
-	log.Printf("[INFO] Updating %s integration with data:\n%+v\n", api.ServiceNowChannelIntegration, serviceNow)
-	response, err := lacework.Integrations.UpdateServiceNowAlertChannel(serviceNow)
+	log.Printf("[INFO] Updating %s integration with data:\n%+v\n", api.ServiceNowRestAlertChannelType, serviceNow)
+	response, err := lacework.V2.AlertChannels.UpdateServiceNowRest(serviceNow)
 	if err != nil {
 		return err
 	}
 
-	log.Println("[INFO] Verifying server response data")
-	err = validateServiceNowAlertChannelResponse(&response)
-	if err != nil {
-		return err
-	}
-
-	integration := response.Data[0]
+	integration := response.Data
 	d.Set("name", integration.Name)
 	d.Set("intg_guid", integration.IntgGuid)
 	d.Set("enabled", integration.Enabled == 1)
 	d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
 	d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
-	d.Set("type_name", integration.TypeName)
+	d.Set("type_name", integration.Type)
 	d.Set("org_level", integration.IsOrg == 1)
 
 	if d.Get("test_integration").(bool) {
-		log.Printf("[INFO] Testing %s integration for guid %s\n", api.ServiceNowChannelIntegration, d.Id())
+		log.Printf("[INFO] Testing %s integration for guid %s\n", api.ServiceNowRestAlertChannelType, d.Id())
 		if err := lacework.V2.AlertChannels.Test(d.Id()); err != nil {
 			return err
 		}
-		log.Printf("[INFO] Tested %s integration with guid %s successfully\n", api.ServiceNowChannelIntegration, d.Id())
+		log.Printf("[INFO] Tested %s integration with guid %s successfully\n", api.ServiceNowRestAlertChannelType, d.Id())
 	}
 
-	log.Printf("[INFO] Updated %s integration with guid %s\n", api.ServiceNowChannelIntegration, d.Id())
+	log.Printf("[INFO] Updated %s integration with guid %s\n", api.ServiceNowRestAlertChannelType, d.Id())
 	return nil
 }
 
 func resourceLaceworkAlertChannelServiceNowDelete(d *schema.ResourceData, meta interface{}) error {
 	lacework := meta.(*api.Client)
 
-	log.Printf("[INFO] Deleting %s integration with guid %s\n", api.ServiceNowChannelIntegration, d.Id())
-	_, err := lacework.Integrations.Delete(d.Id())
+	log.Printf("[INFO] Deleting %s integration with guid %s\n", api.ServiceNowRestAlertChannelType, d.Id())
+	err := lacework.V2.AlertChannels.Delete(d.Id())
 	if err != nil {
 		return err
 	}
 
-	log.Printf("[INFO] Deleted %s integration with guid %s\n", api.ServiceNowChannelIntegration, d.Id())
-	return nil
-}
-
-func validateServiceNowAlertChannelResponse(response *api.ServiceNowAlertChannelResponse) error {
-	if len(response.Data) == 0 {
-		msg := `
-Unable to read sever response data. (empty 'data' field)
-
-This was an unexpected behavior, verify that your integration has been
-created successfully and report this issue to support@lacework.net
-`
-		return fmt.Errorf(msg)
-	}
-
-	if len(response.Data) > 1 {
-		msg := `
-There is more that one integration inside the server response data.
-
-List of integrations:
-`
-		for _, integration := range response.Data {
-			msg = msg + fmt.Sprintf("\t%s: %s\n", integration.IntgGuid, integration.Name)
-		}
-		msg = msg + unexpectedBehaviorMsg()
-		return fmt.Errorf(msg)
-	}
-
+	log.Printf("[INFO] Deleted %s integration with guid %s\n", api.ServiceNowRestAlertChannelType, d.Id())
 	return nil
 }
