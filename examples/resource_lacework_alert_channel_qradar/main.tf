088223f89eb784b8f0f93ee10e09f8b5d97ea94a
Darren Murray <darren.murray@lacework.net>
2021-10-22 10:38:53 +0100

    fix: Migrate IbmQRadar alert channel(v2)
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- examples/resource_lacework_alert_channel_qradar/main.tf
+++ examples/resource_lacework_alert_channel_qradar/main.tf
@@ -1,8 +1,55 @@
-provider "lacework" {}
+terraform {
+  required_providers {
+    lacework = {
+      source = "lacework/lacework"
+    }
+  }
+}
 
 resource "lacework_alert_channel_qradar" "example" {
-  name               = "QRadar Channel Alert Example"
-  host_url           = "https://qradar-lacework.com"
-  host_port          = 4000
-  communication_type = "HTTPS"
+  name               = var.channel_name
+  host_url           = var.host_url
+  host_port          = var.host_port
+  communication_type = var.communication_type
+  // test_integration input is used in this example only for testing
+  // purposes, it help us avoid sending a "test" request to the
+  // system we are integrating to. In production, this should remain
+  // turned on ("true") which is the default setting
+  test_integration = false
+}
+
+variable "channel_name" {
+  type    = string
+  default = "IbmQRadar Alert Channel"
+}
+
+variable "host_url" {
+  type    = string
+  default = "https://qradar-lacework.com"
+}
+
+variable "host_port" {
+  type    = number
+  default = 4000
+}
+
+variable "communication_type" {
+  type    = string
+  default = "HTTPS"
+}
+
+output "channel_name" {
+  value = lacework_alert_channel_qradar.example.name
+}
+
+output "host_url" {
+  value = lacework_alert_channel_qradar.example.host_url
+}
+
+output "host_port" {
+  value = lacework_alert_channel_qradar.example.host_port
+}
+
+output "communication_type" {
+  value = lacework_alert_channel_qradar.example.communication_type
 }
