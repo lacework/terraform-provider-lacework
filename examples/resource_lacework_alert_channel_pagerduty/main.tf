9948a4ca3af13a81d5ae28cc4831e94fc05cad46
Darren Murray <darren.murray@lacework.net>
2021-10-22 10:57:59 +0100

    fix: Migrate PagerDuty alert channel(v2)
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- examples/resource_lacework_alert_channel_pagerduty/main.tf
+++ examples/resource_lacework_alert_channel_pagerduty/main.tf
@@ -1,6 +1,29 @@
 provider "lacework" {}
 
 resource "lacework_alert_channel_pagerduty" "example" {
-  name            = "default alerts"
-  integration_key = "1234abc8901abc567abc123abc78e012"
+  name            = var.channel_name
+  integration_key = var.integration_key
+  // test_integration input is used in this example only for testing
+  // purposes, it help us avoid sending a "test" request to the
+  // system we are integrating to. In production, this should remain
+  // turned on ("true") which is the default setting
+  test_integration = false
+}
+
+variable "channel_name" {
+  type    = string
+  default = "PagerDuty Alert Channel"
+}
+
+variable "integration_key" {
+  type    = string
+  default = "1234abc8901abc567abc123abc78e012"
+}
+
+output "channel_name" {
+  value = lacework_alert_channel_pagerduty.example.name
+}
+
+output "integration_key" {
+  value = lacework_alert_channel_pagerduty.example.integration_key
 }
--- lacework/resource_lacework_alert_channel_pagerduty.go
+++ lacework/resource_lacework_alert_channel_pagerduty.go
@@ -1,7 +1,6 @@
 package lacework
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -17,33 +16,36 @@ func resourceLaceworkAlertChannelPagerDuty() *schema.Resource {
 		Delete: resourceLaceworkAlertChannelPagerDutyDelete,
 
 		Importer: &schema.ResourceImporter{
-			State: importLaceworkIntegration,
+			State: importLaceworkAlertChannel,
 		},
 
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"intg_guid": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The integration name",
 			},
 			"enabled": {
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  true,
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     true,
+				Description: "The state of the external integration",
 			},
 			"integration_key": {
-				Type:      schema.TypeString,
-				Required:  true,
-				Sensitive: true,
+				Type:        schema.TypeString,
+				Required:    true,
+				Sensitive:   true,
+				Description: "The PagerDuty service integration key",
 			},
 			"test_integration": {
 				Type:        schema.TypeBool,
 				Optional:    true,
 				Default:     true,
 				Description: "Whether to test the integration of an alert channel upon creation or modification",
+			},
+			"intg_guid": {
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 			"created_or_updated_time": {
 				Type:     schema.TypeString,
@@ -68,8 +70,9 @@ func resourceLaceworkAlertChannelPagerDuty() *schema.Resource {
 func resourceLaceworkAlertChannelPagerDutyCreate(d *schema.ResourceData, meta interface{}) error {
 	var (
 		lacework = meta.(*api.Client)
-		alert    = api.NewPagerDutyAlertChannel(d.Get("name").(string),
-			api.PagerDutyData{
+		alert    = api.NewAlertChannel(d.Get("name").(string),
+			api.PagerDutyApiAlertChannelType,
+			api.PagerDutyApiDataV2{
 				IntegrationKey: d.Get("integration_key").(string),
 			},
 		)
@@ -78,76 +81,65 @@ func resourceLaceworkAlertChannelPagerDutyCreate(d *schema.ResourceData, meta in
 		alert.Enabled = 0
 	}
 
-	log.Printf("[INFO] Creating %s integration with data:\n%+v\n", api.PagerDutyIntegration, alert)
-	response, err := lacework.Integrations.CreatePagerDutyAlertChannel(alert)
-	if err != nil {
-		return err
-	}
-
-	log.Println("[INFO] Verifying server response data")
-	err = validatePagerDutyAlertChannelResponse(&response)
+	log.Printf("[INFO] Creating %s integration with data:\n%+v\n", api.PagerDutyApiAlertChannelType, alert)
+	response, err := lacework.V2.AlertChannels.Create(alert)
 	if err != nil {
 		return err
 	}
 
 	// @afiune at this point of time, we know the data field has a single value
-	integration := response.Data[0]
+	integration := response.Data
 	d.SetId(integration.IntgGuid)
 	d.Set("name", integration.Name)
 	d.Set("intg_guid", integration.IntgGuid)
 	d.Set("enabled", integration.Enabled == 1)
 	d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
 	d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
-	d.Set("type_name", integration.TypeName)
+	d.Set("type_name", integration.Type)
 	d.Set("org_level", integration.IsOrg == 1)
 
 	if d.Get("test_integration").(bool) {
-		log.Printf("[INFO] Testing %s integration for guid %s\n", api.PagerDutyIntegration, d.Id())
+		log.Printf("[INFO] Testing %s integration for guid %s\n", api.PagerDutyApiAlertChannelType, d.Id())
 		if err := VerifyAlertChannelAndRollback(d, lacework); err != nil {
 			return err
 		}
-		log.Printf("[INFO] Tested %s integration with guid %s successfully\n", api.PagerDutyIntegration, d.Id())
+		log.Printf("[INFO] Tested %s integration with guid %s successfully\n", api.PagerDutyApiAlertChannelType, d.Id())
 	}
 
-	log.Printf("[INFO] Created %s integration with guid %s\n", api.PagerDutyIntegration, integration.IntgGuid)
+	log.Printf("[INFO] Created %s integration with guid %s\n", api.PagerDutyApiAlertChannelType, integration.IntgGuid)
 	return nil
 }
 
 func resourceLaceworkAlertChannelPagerDutyRead(d *schema.ResourceData, meta interface{}) error {
 	lacework := meta.(*api.Client)
 
-	log.Printf("[INFO] Reading %s integration with guid %s\n", api.PagerDutyIntegration, d.Id())
-	response, err := lacework.Integrations.GetPagerDutyAlertChannel(d.Id())
+	log.Printf("[INFO] Reading %s integration with guid %s\n", api.PagerDutyApiAlertChannelType, d.Id())
+	response, err := lacework.V2.AlertChannels.GetPagerDutyApi(d.Id())
 	if err != nil {
 		return err
 	}
 
-	for _, integration := range response.Data {
-		if integration.IntgGuid == d.Id() {
-			d.Set("name", integration.Name)
-			d.Set("intg_guid", integration.IntgGuid)
-			d.Set("enabled", integration.Enabled == 1)
-			d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
-			d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
-			d.Set("type_name", integration.TypeName)
-			d.Set("org_level", integration.IsOrg == 1)
-			d.Set("integration_key", integration.Data.IntegrationKey)
+	integration := response.Data
+	d.Set("name", integration.Name)
+	d.Set("intg_guid", integration.IntgGuid)
+	d.Set("enabled", integration.Enabled == 1)
+	d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
+	d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
+	d.Set("type_name", integration.Type)
+	d.Set("org_level", integration.IsOrg == 1)
+	d.Set("integration_key", integration.Data.IntegrationKey)
 
-			log.Printf("[INFO] Read %s integration with guid %s\n",
-				api.PagerDutyIntegration, integration.IntgGuid)
-			return nil
-		}
-	}
-
-	d.SetId("")
+	log.Printf("[INFO] Read %s integration with guid %s\n",
+		api.PagerDutyApiAlertChannelType, integration.IntgGuid)
 	return nil
 }
 
 func resourceLaceworkAlertChannelPagerDutyUpdate(d *schema.ResourceData, meta interface{}) error {
 	var (
 		lacework = meta.(*api.Client)
-		alert    = api.NewPagerDutyAlertChannel(d.Get("name").(string),
-			api.PagerDutyData{
+		alert    = api.NewAlertChannel(d.Get("name").(string),
+			api.PagerDutyApiAlertChannelType,
+			api.PagerDutyApiDataV2{
 				IntegrationKey: d.Get("integration_key").(string),
 			},
 		)
@@ -159,85 +151,43 @@ func resourceLaceworkAlertChannelPagerDutyUpdate(d *schema.ResourceData, meta in
 
 	alert.IntgGuid = d.Id()
 
-	log.Printf("[INFO] Updating %s integration with data:\n%+v\n", api.PagerDutyIntegration, alert)
-	response, err := lacework.Integrations.UpdatePagerDutyAlertChannel(alert)
-	if err != nil {
-		return err
-	}
-
-	log.Println("[INFO] Verifying server response data")
-	err = validatePagerDutyAlertChannelResponse(&response)
+	log.Printf("[INFO] Updating %s integration with data:\n%+v\n", api.PagerDutyApiAlertChannelType, alert)
+	response, err := lacework.V2.AlertChannels.UpdatePagerDutyApi(alert)
 	if err != nil {
 		return err
 	}
 
 	// @afiune at this point of time, we know the data field has a single value
-	integration := response.Data[0]
+	integration := response.Data
 	d.Set("name", integration.Name)
 	d.Set("intg_guid", integration.IntgGuid)
 	d.Set("enabled", integration.Enabled == 1)
 	d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
 	d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
-	d.Set("type_name", integration.TypeName)
+	d.Set("type_name", integration.Type)
 	d.Set("org_level", integration.IsOrg == 1)
 
 	if d.Get("test_integration").(bool) {
-		log.Printf("[INFO] Testing %s integration for guid %s\n", api.PagerDutyIntegration, d.Id())
+		log.Printf("[INFO] Testing %s integration for guid %s\n", api.PagerDutyApiAlertChannelType, d.Id())
 		if err := lacework.V2.AlertChannels.Test(d.Id()); err != nil {
 			return err
 		}
-		log.Printf("[INFO] Tested %s integration with guid %s successfully\n", api.PagerDutyIntegration, d.Id())
+		log.Printf("[INFO] Tested %s integration with guid %s successfully\n", api.PagerDutyApiAlertChannelType, d.Id())
 	}
 
-	log.Printf("[INFO] Updated %s integration with guid %s\n", api.PagerDutyIntegration, d.Id())
+	log.Printf("[INFO] Updated %s integration with guid %s\n", api.PagerDutyApiAlertChannelType, d.Id())
 	return nil
 }
 
 func resourceLaceworkAlertChannelPagerDutyDelete(d *schema.ResourceData, meta interface{}) error {
 	lacework := meta.(*api.Client)
 
-	log.Printf("[INFO] Deleting %s integration with guid %s\n", api.PagerDutyIntegration, d.Id())
-	_, err := lacework.Integrations.Delete(d.Id())
+	log.Printf("[INFO] Deleting %s integration with guid %s\n", api.PagerDutyApiAlertChannelType, d.Id())
+	err := lacework.V2.AlertChannels.Delete(d.Id())
 	if err != nil {
 		return err
 	}
 
-	log.Printf("[INFO] Deleted %s integration with guid %s\n", api.PagerDutyIntegration, d.Id())
-	return nil
-}
-
-// validatePagerDutyAlertChannelResponse checks weather or not the server response has
-// any inconsistent data, it returns a friendly error message describing the
-// problem and how to report it
-func validatePagerDutyAlertChannelResponse(response *api.PagerDutyAlertChannelResponse) error {
-	if len(response.Data) == 0 {
-		// @afiune this edge case should never happen, if we land here it means that
-		// something went wrong in the server side of things (Lacework API), so let
-		// us inform that to our users
-		msg := `
-Unable to read sever response data. (empty 'data' field)
-
-This was an unexpected behavior, verify that your integration has been
-created successfully and report this issue to support@lacework.net
-`
-		return fmt.Errorf(msg)
-	}
-
-	if len(response.Data) > 1 {
-		// @afiune if we are creating a single integration and the server returns
-		// more than one integration inside the 'data' field, it is definitely another
-		// edge case that should never happen
-		msg := `
-There is more that one integration inside the server response data.
-
-List of integrations:
-`
-		for _, integration := range response.Data {
-			msg = msg + fmt.Sprintf("\t%s: %s\n", integration.IntgGuid, integration.Name)
-		}
-		msg = msg + unexpectedBehaviorMsg()
-		return fmt.Errorf(msg)
-	}
-
+	log.Printf("[INFO] Deleted %s integration with guid %s\n", api.PagerDutyApiAlertChannelType, d.Id())
 	return nil
 }
