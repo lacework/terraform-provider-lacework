95787032eb297708c57f28609912741d1638832e
Salim Afiune Maya <afiune@lacework.net>
2021-12-15 02:32:24 -0600

    test: TeamMembers org and standalone
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- examples/resource_lacework_team_member_standalone/main.tf
+++ examples/resource_lacework_team_member_standalone/main.tf
@@ -9,8 +9,29 @@ terraform {
 provider "lacework" {}
 
 resource "lacework_team_member" "example" {
-  email      = "vatasha.white+2@lacework.net"
-  first_name = "Vatasha"
-  last_name  = "White"
-  company    = "Pokemon International Company"
+  email         = var.email
+  first_name    = var.first_name
+  last_name     = var.last_name
+  company       = "Marvel Comics"
+  administrator = var.administrator
+}
+
+variable "email" {
+  type    = string
+  default = "vatasha.white+2@lacework.net"
+}
+
+variable "first_name" {
+  type    = string
+  default = "Shuri"
+}
+
+variable "last_name" {
+  type    = string
+  default = "White"
+}
+
+variable "administrator" {
+  type    = bool
+  default = false
 }
