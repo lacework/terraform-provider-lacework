4aca29718baa78c766c8b28e3a33c6906e50f87a
Darren Murray <darren.murray@lacework.net>
2022-02-14 14:32:49 +0000

    feat: new lacework_policy resource
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

---
+++ examples/resource_lacework_policy/main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    lacework = {
+      source = "lacework/lacework"
+    }
+  }
+}
+
+resource "lacework_policy" "example" {
+  title            = var.title
+  query_id         = "LW_Global_AWS_CTA_CloudTrailChange"
+  severity         = var.severity
+  type             = var.type
+  description      = var.description
+  remediation      = var.remediation
+  evaluation       = var.evaluation
+  evaluator_id     = "Cloudtrail"
+  enabled          = true
+  policy_id_suffix = var.policy_id_suffix
+
+  alerting {
+    enabled = false
+    profile = "LW_CloudTrail_Alerts"
+  }
+}
+
+
+variable "title" {
+  type    = string
+  default = "lql-terraform-policy"
+}
+
+variable "description" {
+  type    = string
+  default = "Policy Created via Terraform"
+}
+
+variable "severity" {
+  type    = string
+  default = "High"
+}
+
+variable "remediation" {
+  type    = string
+  default = "Please Investigate"
+}
+
+variable "type" {
+  type    = string
+  default = "Violation"
+}
+
+variable "evaluation" {
+  type    = string
+  default = "Hourly"
+}
+
+variable "policy_id_suffix" {
+  default = ""
+}
+
+output "title" {
+  value = lacework_policy.example.title
+}
+
+output "severity" {
+  value = lacework_policy.example.severity
+}
+
+output "remediation" {
+  value = lacework_policy.example.remediation
+}
+
+output "evaluation" {
+  value = lacework_policy.example.evaluation
+}
+
+output "type" {
+  value = lacework_policy.example.type
+}
+
+output "description" {
+  value = lacework_policy.example.description
+}
+
+output "policy_id_suffix" {
+  value = lacework_policy.example.policy_id_suffix
+}
+
