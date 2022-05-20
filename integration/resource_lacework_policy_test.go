d15112e3832c61d7a0be8dcb050b2e98d03e6149
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2022-05-20 13:48:36 +0100

    test: fix policy test (#312)
    * test: fix policy test
    
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- integration/resource_lacework_policy_test.go
+++ integration/resource_lacework_policy_test.go
@@ -49,7 +49,7 @@ func TestPolicyCreate(t *testing.T) {
 	assert.Contains(t, "Policy Created via Terraform", createProps.Data.Description)
 	assert.Contains(t, "Please Investigate", createProps.Data.Remediation)
 	assert.Contains(t, "Hourly", createProps.Data.EvalFrequency)
-	assert.Equal(t, []string{"domain:AWS", "subdomain:Cloudtrail"}, createProps.Data.Tags)
+	assert.ElementsMatch(t, []string{"domain:AWS", "subdomain:Cloudtrail"}, createProps.Data.Tags)
 
 	assert.Equal(t, "lql-terraform-policy", actualTitle)
 	assert.Equal(t, "high", actualSeverity)
@@ -87,7 +87,7 @@ func TestPolicyCreate(t *testing.T) {
 	assert.Contains(t, "Policy Created via Terraform Updated", updateProps.Data.Description)
 	assert.Contains(t, "Please Ignore", updateProps.Data.Remediation)
 	assert.Contains(t, "Daily", updateProps.Data.EvalFrequency)
-	assert.Equal(t, []string{"custom", "domain:AWS", "subdomain:Cloudtrail"}, updateProps.Data.Tags)
+	assert.ElementsMatch(t, []string{"custom", "domain:AWS", "subdomain:Cloudtrail"}, updateProps.Data.Tags)
 
 	assert.Equal(t, "lql-terraform-policy-updated", actualTitle)
 	assert.Equal(t, "low", actualSeverity)
