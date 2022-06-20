6658e1101b042411de98f47a83bcb8ef42475b1d
Ross <ross.moles@lacework.net>
2022-06-20 15:33:30 +0100

    feat(GcpGkeAudit): Update GcpGkeAudit integration tests to use google creds
    Signed-off-by: Ross <ross.moles@lacework.net>

--- integration/resource_lacework_integration_gcp_gke_audit_log_test.go
+++ integration/resource_lacework_integration_gcp_gke_audit_log_test.go
@@ -13,88 +13,77 @@ import (
 // It uses the go-sdk to verify the created integration,
 // applies an update with new integration name and destroys it
 func TestIntegrationGcpGkeAuditLog(t *testing.T) {
-	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../examples/resource_lacework_integration_gcp_gke_audit_log",
-		Vars: map[string]interface{}{
-			"client_id":        "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-			"client_email":     "email@some-project-name.iam.gserviceaccount.com",
-			"private_key_id":   "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-			"private_key":      "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	gcreds, err := googleLoadDefaultCredentials()
+	if assert.Nil(t, err, "this test requires you to set GOOGLE_CREDENTIALS environment variable") {
+		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+			TerraformDir: "../examples/resource_lacework_integration_gcp_gke_audit_log",
+			Vars: map[string]interface{}{
+				"client_id":        gcreds.ClientID,
+				"client_email":     gcreds.ClientEmail,
+				"private_key_id":   gcreds.PrivateKeyID,
+				"private_key":      gcreds.PrivateKey,
+				"integration_type": "PROJECT",
+				"project_id":       "example_project_id",
+				"subscription":     "projects/example-project-id/subscriptions/example-subscription",
+			},
+		})
+		defer terraform.Destroy(t, terraformOptions)
+
+		// Create new GcpGkeAudit Integration
+		create := terraform.InitAndApplyAndIdempotent(t, terraformOptions)
+		actualClientId := terraform.Output(t, terraformOptions, "client_id")
+		actualClientEmail := terraform.Output(t, terraformOptions, "client_email")
+		actualPrivateKey := terraform.Output(t, terraformOptions, "private_key")
+		actualPrivateKeyId := terraform.Output(t, terraformOptions, "private_key_id")
+		actualIntegrationType := terraform.Output(t, terraformOptions, "integration_type")
+		actualProjectId := terraform.Output(t, terraformOptions, "project_id")
+		actualSubscription := terraform.Output(t, terraformOptions, "subscription")
+		assert.Equal(
+			t,
+			"GCP GKE audit log integration example",
+			GetCloudAccountIntegrationName(create),
+		)
+		assert.Equal(t, gcreds.ClientID, actualClientId)
+		assert.Equal(t, gcreds.ClientEmail, actualClientEmail)
+		assert.Equal(t, "", actualPrivateKey)
+		assert.Equal(t, gcreds.PrivateKeyID, actualPrivateKeyId)
+		assert.Equal(t, "PROJECT", actualIntegrationType)
+		assert.Equal(t, "example_project_id", actualProjectId)
+		assert.Equal(t, "projects/example-project-id/subscriptions/example-subscription", actualSubscription)
+
+		// Update GcpGkeAudit Integration
+		terraformOptions.Vars = map[string]interface{}{
+			"name":             "GcpGkeAudit log integration updated",
+			"client_id":        gcreds.ClientID,
+			"client_email":     gcreds.ClientEmail,
+			"private_key_id":   gcreds.PrivateKeyID,
+			"private_key":      gcreds.PrivateKey,
 			"integration_type": "PROJECT",
 			"project_id":       "example_project_id",
 			"subscription":     "projects/example-project-id/subscriptions/example-subscription",
-		},
-	})
-	defer terraform.Destroy(t, terraformOptions)
+		}
 
-	// Create new GcpGkeAudit Integration
-	create := terraform.InitAndApplyAndIdempotent(t, terraformOptions)
-	actualClientId := terraform.Output(t, terraformOptions, "client_id")
-	actualClientEmail := terraform.Output(t, terraformOptions, "client_email")
-	actualPrivateKey := terraform.Output(t, terraformOptions, "private_key")
-	actualPrivateKeyId := terraform.Output(t, terraformOptions, "private_key_id")
-	actualIntegrationType := terraform.Output(t, terraformOptions, "integration_type")
-	actualProjectId := terraform.Output(t, terraformOptions, "project_id")
-	actualSubscription := terraform.Output(t, terraformOptions, "subscription")
-	assert.Equal(
-		t,
-		"GCP GKE audit log integration example",
-		GetCloudAccountIntegrationName(create),
-	)
-	assert.Equal(
-		t,
-		"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-		actualClientId,
-	)
-	assert.Equal(
-		t,
-		"email@some-project-name.iam.gserviceaccount.com",
-		actualClientEmail,
-	)
-	assert.Equal(t, "", actualPrivateKey)
-	assert.Equal(t, "", actualPrivateKeyId)
-	assert.Equal(t, "PROJECT", actualIntegrationType)
-	assert.Equal(t, "example_project_id", actualProjectId)
-	assert.Equal(t, "projects/example-project-id/subscriptions/example-subscription", actualSubscription)
-
-	// Update GcpGkeAudit Integration
-	terraformOptions.Vars = map[string]interface{}{
-		"name":             "GcpGkeAudit log integration updated",
-		"client_id":        "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-		"client_email":     "email@some-project-name.iam.gserviceaccount.com",
-		"private_key_id":   "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-		"private_key":      "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-		"integration_type": "PROJECT",
-		"project_id":       "example_project_id",
-		"subscription":     "projects/example-project-id/subscriptions/example-subscription",
+		update := terraform.ApplyAndIdempotent(t, terraformOptions)
+		actualClientId = terraform.Output(t, terraformOptions, "client_id")
+		actualClientEmail = terraform.Output(t, terraformOptions, "client_email")
+		actualPrivateKey = terraform.Output(t, terraformOptions, "private_key")
+		actualPrivateKeyId = terraform.Output(t, terraformOptions, "private_key_id")
+		actualIntegrationType = terraform.Output(t, terraformOptions, "integration_type")
+		actualProjectId = terraform.Output(t, terraformOptions, "project_id")
+		actualSubscription = terraform.Output(t, terraformOptions, "subscription")
+		assert.Equal(
+			t,
+			"GcpGkeAudit log integration updated",
+			GetCloudAccountIntegrationName(update),
+		)
+		assert.Equal(t, gcreds.ClientID, actualClientId)
+		assert.Equal(t, gcreds.ClientEmail, actualClientEmail)
+		assert.Equal(t, "", actualPrivateKey)
+		assert.Equal(t, gcreds.PrivateKeyID, actualPrivateKeyId)
+		assert.Equal(t, "", actualPrivateKey)
+		assert.Equal(t, "", actualPrivateKeyId)
+		assert.Equal(t, "PROJECT", actualIntegrationType)
+		assert.Equal(t, "example_project_id", actualProjectId)
+		assert.Equal(t, "projects/example-project-id/subscriptions/example-subscription", actualSubscription)
 	}
-
-	update := terraform.ApplyAndIdempotent(t, terraformOptions)
-	actualClientId = terraform.Output(t, terraformOptions, "client_id")
-	actualClientEmail = terraform.Output(t, terraformOptions, "client_email")
-	actualPrivateKey = terraform.Output(t, terraformOptions, "private_key")
-	actualPrivateKeyId = terraform.Output(t, terraformOptions, "private_key_id")
-	actualIntegrationType = terraform.Output(t, terraformOptions, "integration_type")
-	actualProjectId = terraform.Output(t, terraformOptions, "project_id")
-	actualSubscription = terraform.Output(t, terraformOptions, "subscription")
-	assert.Equal(
-		t,
-		"GcpGkeAudit log integration updated",
-		GetCloudAccountIntegrationName(update),
-	)
-	assert.Equal(
-		t,
-		"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-		actualClientId,
-	)
-	assert.Equal(
-		t,
-		"email@some-project-name.iam.gserviceaccount.com",
-		actualClientEmail,
-	)
-	assert.Equal(t, "", actualPrivateKey)
-	assert.Equal(t, "", actualPrivateKeyId)
-	assert.Equal(t, "PROJECT", actualIntegrationType)
-	assert.Equal(t, "example_project_id", actualProjectId)
-	assert.Equal(t, "projects/example-project-id/subscriptions/example-subscription", actualSubscription)
 }
