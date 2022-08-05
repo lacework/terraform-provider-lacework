bc50d4ad01786a04a52b1dcf4d6e341443032b57
Darren Murray <darren.murray@lacework.net>
2022-08-05 14:03:42 +0100

    test: fix tests
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- integration/integration.go
+++ integration/integration.go
@@ -83,10 +83,10 @@ func GetCloudAccountEksAuditLogData(result string) api.AwsEksAuditData {
 	return response.Data.Data
 }
 
-func GetCloudAccountAgentlessScanningResponse(result string) api.AwsAgentlessScanningResponse {
+func GetCloudAccountAgentlessScanningResponse(result string) api.AwsSidekickResponse {
 	id := GetIDFromTerraResults(result)
 
-	response, err := LwClient.V2.CloudAccounts.GetAwsAgentlessScanning(id)
+	response, err := LwClient.V2.CloudAccounts.GetAwsSidekick(id)
 	if err != nil {
 		log.Fatalf("Unable to find eks audit log id: %s\n Response: %v", id, response)
 	}
