6d66f94f149617ff2340bad3840463caefb5d14d
Darren Murray <darren.murray@lacework.net>
2022-08-09 16:51:10 +0100

    docs: update docs
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- examples/resource_lacework_integration_aws_agentless_scanning/main.tf
+++ examples/resource_lacework_integration_aws_agentless_scanning/main.tf
@@ -12,6 +12,32 @@ resource "lacework_integration_aws_agentless_scanning" "example" {
   scan_frequency            = 24
   scan_containers           = true
   scan_host_vulnerabilities = true
+  account_id = var.account_id
+  bucket_arn = var.bucket_arn
+  credentials {
+    role_arn = var.role_arn
+    external_id = var.external_id
+  }
+}
+
+variable "account_id" {
+  type    = string
+  default = ""
+}
+
+variable "bucket_arn" {
+  type    = string
+  default = ""
+}
+
+variable "role_arn" {
+  type    = string
+  default = ""
+}
+
+variable "external_id" {
+  type    = string
+  default = ""
 }
 
 variable "name" {
