ca5e373ac9af70dc8ad62c7c4e27c942cf3759bd
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2022-09-29 10:36:23 +0100

    refactor: migrate resource_lacework_integration_aws_govcloud_ct to use v2 api (#376)
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- lacework/resource_lacework_integration_aws_govcloud_ct.go
+++ lacework/resource_lacework_integration_aws_govcloud_ct.go
@@ -96,14 +96,14 @@ func resourceLaceworkIntegrationAwsGovCloudCTCreate(d *schema.ResourceData, meta
 	var (
 		lacework = meta.(*api.Client)
 		retries  = d.Get("retries").(int)
-		aws      = api.NewAwsIntegration(d.Get("name").(string),
-			api.AwsGovCloudCTIntegration,
-			api.AwsIntegrationData{
+		aws      = api.NewCloudAccount(d.Get("name").(string),
+			api.AwsUsGovCtSqsCloudAccount,
+			api.AwsUsGovCtSqsData{
 				QueueUrl: d.Get("queue_url").(string),
-				GovCloudCredentials: &api.AwsGovCloudCreds{
+				Credentials: api.AwsUsGovCtSqsCredentials{
 					AccessKeyID:     d.Get("credentials.0.access_key_id").(string),
 					SecretAccessKey: d.Get("credentials.0.secret_access_key").(string),
-					AccountID:       d.Get("account_id").(string),
+					AwsAccountID:    d.Get("account_id").(string),
 				},
 			},
 		)
@@ -115,32 +115,26 @@ func resourceLaceworkIntegrationAwsGovCloudCTCreate(d *schema.ResourceData, meta
 
 	return resource.RetryContext(context.Background(), d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
 		retries--
-		log.Printf("[INFO] Creating %s integration\n", api.AwsGovCloudCTIntegration.String())
-		response, err := lacework.Integrations.CreateAws(aws)
+		log.Printf("[INFO] Creating %s integration\n", api.AwsUsGovCtSqsCloudAccount.String())
+		response, err := lacework.V2.CloudAccounts.Create(aws)
 		if err != nil {
 			if retries <= 0 {
 				return resource.NonRetryableError(
 					fmt.Errorf("Error creating %s integration: %s",
-						api.AwsGovCloudCTIntegration.String(), err,
+						api.AwsUsGovCtSqsCloudAccount.String(), err,
 					))
 			}
 			log.Printf(
 				"[INFO] Unable to create %s integration. (retrying %d more time(s))\n%s\n",
-				api.AwsGovCloudCTIntegration.String(), retries, err,
+				api.AwsUsGovCtSqsCloudAccount.String(), retries, err,
 			)
 			return resource.RetryableError(fmt.Errorf(
 				"Unable to create %s integration (retrying %d more time(s))",
-				api.AwsGovCloudCTIntegration.String(), retries,
+				api.AwsUsGovCtSqsCloudAccount.String(), retries,
 			))
 		}
 
-		log.Printf("[INFO] Verifying server response")
-		err = validateAwsIntegrationResponse(&response)
-		if err != nil {
-			return resource.NonRetryableError(err)
-		}
-
-		integration := response.Data[0]
+		integration := response.Data
 		d.SetId(integration.IntgGuid)
 		d.Set("name", integration.Name)
 		d.Set("intg_guid", integration.IntgGuid)
@@ -148,11 +142,11 @@ func resourceLaceworkIntegrationAwsGovCloudCTCreate(d *schema.ResourceData, meta
 
 		d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
 		d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
-		d.Set("type_name", integration.TypeName)
+		d.Set("type_name", integration.Type)
 		d.Set("org_level", integration.IsOrg == 1)
 
 		log.Printf("[INFO] Created %s integration with guid: %v\n",
-			api.AwsGovCloudCTIntegration.String(), integration.IntgGuid)
+			api.AwsUsGovCtSqsCloudAccount.String(), integration.IntgGuid)
 		return nil
 	})
 }
@@ -161,34 +155,33 @@ func resourceLaceworkIntegrationAwsGovCloudCTRead(d *schema.ResourceData, meta i
 	lacework := meta.(*api.Client)
 
 	log.Printf("[INFO] Reading %s integration with guid: %v\n",
-		api.AwsGovCloudCTIntegration.String(), d.Id())
-	response, err := lacework.Integrations.GetAws(d.Id())
+		api.AwsUsGovCtSqsCloudAccount.String(), d.Id())
+	response, err := lacework.V2.CloudAccounts.GetAwsUsGovCtSqs(d.Id())
 	if err != nil {
 		return resourceNotFound(d, err)
 	}
 
-	for _, integration := range response.Data {
-		if integration.IntgGuid == d.Id() {
-			d.Set("name", integration.Name)
-			d.Set("intg_guid", integration.IntgGuid)
-			d.Set("enabled", integration.Enabled == 1)
-			d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
-			d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
-			d.Set("type_name", integration.TypeName)
-			d.Set("org_level", integration.IsOrg == 1)
-			d.Set("queue_url", integration.Data.QueueUrl)
-			d.Set("account_id", integration.Data.GetAccountID())
+	integration := response.Data
+	if integration.IntgGuid == d.Id() {
+		d.Set("name", integration.Name)
+		d.Set("intg_guid", integration.IntgGuid)
+		d.Set("enabled", integration.Enabled == 1)
+		d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
+		d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
+		d.Set("type_name", integration.Type)
+		d.Set("org_level", integration.IsOrg == 1)
+		d.Set("queue_url", integration.Data.QueueUrl)
+		d.Set("account_id", integration.Data.Credentials.AwsAccountID)
 
-			creds := make(map[string]string)
-			credentials := integration.Data.GetGovCloudCredentials()
-			creds["access_key_id"] = credentials.AccessKeyID
-			creds["secret_access_key"] = credentials.SecretAccessKey
-			d.Set("credentials", []map[string]string{creds})
+		creds := make(map[string]string)
+		credentials := integration.Data.Credentials
+		creds["access_key_id"] = credentials.AccessKeyID
+		creds["secret_access_key"] = credentials.SecretAccessKey
+		d.Set("credentials", []map[string]string{creds})
 
-			log.Printf("[INFO] Read %s integration with guid: %v\n",
-				api.AwsGovCloudCTIntegration.String(), integration.IntgGuid)
-			return nil
-		}
+		log.Printf("[INFO] Read %s integration with guid: %v\n",
+			api.AwsUsGovCtSqsCloudAccount.String(), integration.IntgGuid)
+		return nil
 	}
 
 	d.SetId("")
@@ -198,14 +191,14 @@ func resourceLaceworkIntegrationAwsGovCloudCTRead(d *schema.ResourceData, meta i
 func resourceLaceworkIntegrationAwsGovCloudCTUpdate(d *schema.ResourceData, meta interface{}) error {
 	var (
 		lacework = meta.(*api.Client)
-		aws      = api.NewAwsIntegration(d.Get("name").(string),
-			api.AwsGovCloudCTIntegration,
-			api.AwsIntegrationData{
+		aws      = api.NewCloudAccount(d.Get("name").(string),
+			api.AwsUsGovCtSqsCloudAccount,
+			api.AwsUsGovCtSqsData{
 				QueueUrl: d.Get("queue_url").(string),
-				GovCloudCredentials: &api.AwsGovCloudCreds{
+				Credentials: api.AwsUsGovCtSqsCredentials{
 					AccessKeyID:     d.Get("credentials.0.access_key_id").(string),
 					SecretAccessKey: d.Get("credentials.0.secret_access_key").(string),
-					AccountID:       d.Get("account_id").(string),
+					AwsAccountID:    d.Get("account_id").(string),
 				},
 			},
 		)
@@ -218,29 +211,23 @@ func resourceLaceworkIntegrationAwsGovCloudCTUpdate(d *schema.ResourceData, meta
 	aws.IntgGuid = d.Id()
 
 	log.Printf("[INFO] Updating %s integration with data:\n%+v\n",
-		api.AwsGovCloudCTIntegration.String(), aws)
-	response, err := lacework.Integrations.UpdateAws(aws)
+		api.AwsUsGovCtSqsCloudAccount.String(), aws)
+	response, err := lacework.V2.CloudAccounts.UpdateAwsUsGovCtSqs(aws)
 	if err != nil {
 		return err
 	}
 
-	log.Println("[INFO] Verifying server response data")
-	err = validateAwsIntegrationResponse(&response)
-	if err != nil {
-		return err
-	}
-
-	integration := response.Data[0]
+	integration := response.Data
 	d.Set("name", integration.Name)
 	d.Set("intg_guid", integration.IntgGuid)
 	d.Set("enabled", integration.Enabled == 1)
 	d.Set("created_or_updated_time", integration.CreatedOrUpdatedTime)
 	d.Set("created_or_updated_by", integration.CreatedOrUpdatedBy)
-	d.Set("type_name", integration.TypeName)
+	d.Set("type_name", integration.Type)
 	d.Set("org_level", integration.IsOrg == 1)
 
 	log.Printf("[INFO] Updated %s integration with guid: %v\n",
-		api.AwsGovCloudCTIntegration.String(), d.Id())
+		api.AwsUsGovCtSqsCloudAccount.String(), d.Id())
 	return nil
 }
 
@@ -248,13 +235,13 @@ func resourceLaceworkIntegrationAwsGovCloudCTDelete(d *schema.ResourceData, meta
 	lacework := meta.(*api.Client)
 
 	log.Printf("[INFO] Deleting %s integration with guid: %v\n",
-		api.AwsGovCloudCTIntegration.String(), d.Id())
-	_, err := lacework.Integrations.DeleteAws(d.Id())
+		api.AwsUsGovCtSqsCloudAccount.String(), d.Id())
+	err := lacework.V2.CloudAccounts.Delete(d.Id())
 	if err != nil {
 		return err
 	}
 
 	log.Printf("[INFO] Deleted %s integration with guid: %v\n",
-		api.AwsGovCloudCTIntegration.String(), d.Id())
+		api.AwsUsGovCtSqsCloudAccount.String(), d.Id())
 	return nil
 }
