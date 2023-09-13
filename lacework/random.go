fe568c8af6087e0e566adabc9cc41632fe746d88
David McTavish <david.mctavish@lacework.net>
2023-09-13 11:58:09 +0100

    feat(data_source): fix lint error

--- lacework/random.go
+++ lacework/random.go
@@ -5,16 +5,13 @@ import (
 	"time"
 )
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
 var letters = []rune("=,.@:/-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 
 func randString(lenght int) string {
+	var rand_gen = rand.New(rand.NewSource(time.Now().UnixNano()))
 	r := make([]rune, lenght)
 	for i := range r {
-		r[i] = letters[rand.Intn(len(letters))]
+		r[i] = letters[rand_gen.Intn(len(letters))]
 	}
 	return string(r)
 }
