14956457c0a41104428970402ab09603daab7e6f
David McTavish <david.mctavish@lacework.net>
2023-09-13 12:05:43 +0100

    feat(data_source): tidy up

--- lacework/random.go
+++ lacework/random.go
@@ -8,7 +8,7 @@ import (
 var letters = []rune("=,.@:/-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 
 func randString(lenght int) string {
-	var rand_gen = rand.New(rand.NewSource(time.Now().UnixNano()))
+	rand_gen := rand.New(rand.NewSource(time.Now().UnixNano()))
 	r := make([]rune, lenght)
 	for i := range r {
 		r[i] = letters[rand_gen.Intn(len(letters))]
