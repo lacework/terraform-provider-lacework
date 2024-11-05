045ab47216a897136b27d5eb36f8c606b3033cd2
Lei Jin <lei.jin@lacework.net>
2024-11-05 18:26:39 +0000

    Fix: Enable azure agentless test
    Signed-off-by: Lei Jin <lei.jin@lacework.net>

--- integration/resource_lacework_integration_azure_agentless_scanning_test.go
+++ integration/resource_lacework_integration_azure_agentless_scanning_test.go
@@ -1,46 +1,46 @@
 package integration
 
-//import (
-//	"fmt"
-//	"testing"
-//
-//	"github.com/gruntwork-io/terratest/modules/terraform"
-//	"github.com/stretchr/testify/assert"
-//)
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
 
 // TestIntegrationAzureAgentlessScanningCreate applies integration terraform:
 // => '../examples/resource_lacework_integration_azure_agentless_scanning'
 //
 // It uses the go-sdk to verify the created integration,
 // applies an update with new integration name and destroys it
-//func TestIntegrationAzureAgentlessScanningCreate(t *testing.T) {
-//	credential, err := azureLoadDefaultCredentials()
-//	integration_name := "Azure Agentless Scanning Example Integration Test"
-//	update_integration_name := fmt.Sprintf("%s Updated", integration_name)
-//	if assert.Nil(t, err, "this test requires you to set AZURE_CREDENTIALS environment variable") {
-//		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//			TerraformDir: "../examples/resource_lacework_integration_azure_agentless_scanning",
-//			Vars: map[string]interface{}{
-//				"integration_name": integration_name,
-//				"client_id":        credential.ClientID,
-//				"client_secret":    credential.ClientSecret,
-//			},
-//			EnvVars: map[string]string{
-//				"LW_API_TOKEN": LwApiToken,
-//			},
-//		})
-//		defer terraform.Destroy(t, terraformOptions)
-//
-//		// Create new Azure Agentless Scanning integration
-//		create := terraform.InitAndApplyAndIdempotent(t, terraformOptions)
-//		createData := GetAzureAgentlessScanningResponse(create)
-//		assert.Equal(t, integration_name, createData.Data.Name)
-//
-//		// Update Azure integration
-//		terraformOptions.Vars["integration_name"] = update_integration_name
-//
-//		update := terraform.InitAndApplyAndIdempotent(t, terraformOptions)
-//		updateData := GetAzureAgentlessScanningResponse(update)
-//		assert.Equal(t, update_integration_name, updateData.Data.Name)
-//	}
-//}
+func TestIntegrationAzureAgentlessScanningCreate(t *testing.T) {
+	credential, err := azureLoadDefaultCredentials()
+	integration_name := "Azure Agentless Scanning Example Integration Test"
+	update_integration_name := fmt.Sprintf("%s Updated", integration_name)
+	if assert.Nil(t, err, "this test requires you to set AZURE_CREDENTIALS environment variable") {
+		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+			TerraformDir: "../examples/resource_lacework_integration_azure_agentless_scanning",
+			Vars: map[string]interface{}{
+				"integration_name": integration_name,
+				"client_id":        credential.ClientID,
+				"client_secret":    credential.ClientSecret,
+			},
+			EnvVars: map[string]string{
+				"LW_API_TOKEN": LwApiToken,
+			},
+		})
+		defer terraform.Destroy(t, terraformOptions)
+
+		// Create new Azure Agentless Scanning integration
+		create := terraform.InitAndApplyAndIdempotent(t, terraformOptions)
+		createData := GetAzureAgentlessScanningResponse(create)
+		assert.Equal(t, integration_name, createData.Data.Name)
+
+		// Update Azure integration
+		terraformOptions.Vars["integration_name"] = update_integration_name
+
+		update := terraform.InitAndApplyAndIdempotent(t, terraformOptions)
+		updateData := GetAzureAgentlessScanningResponse(update)
+		assert.Equal(t, update_integration_name, updateData.Data.Name)
+	}
+}
