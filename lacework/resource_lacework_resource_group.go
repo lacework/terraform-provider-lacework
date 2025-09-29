2703ceb36f0c4c8f57acebc39c53ce32920837d4
Pengyuan Zhao <PengyuanZhao@users.noreply.github.com>
2025-09-29 14:42:50 -0400

    fix: add Computed: True to resource_group enabled (#706)

--- lacework/resource_lacework_resource_group.go
+++ lacework/resource_lacework_resource_group.go
@@ -126,7 +126,7 @@ func resourceLaceworkResourceGroup() *schema.Resource {
 			"enabled": {
 				Type:        schema.TypeBool,
 				Optional:    true,
-				Default:     true,
+				Computed:    true,
 				Description: "The state of the resource group",
 			},
 			"description": {
